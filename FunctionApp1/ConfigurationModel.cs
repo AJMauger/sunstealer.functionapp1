f31f7f64f86419d4544d0c8bb380bea362915b23
My name <40044228+AJMauger@users.noreply.github.com>
2024-03-25 19:21:16 -0600

    Add files via upload

---
+++ FunctionApp1/ConfigurationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FunctionApp1.Models;
+
+public class ConfigurationModel
+{
+    public static string env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Error";
+
+    public string DatabaseConnectionString { get; set; } = string.Empty;
+}
---
+++ FunctionApp1/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using FunctionApp1;
+using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using FunctionApp1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var appConfig = builder.Configuration.GetConnectionString("AppConfig");
+var myValue = builder.Configuration.GetValue<string>("MyKey");
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication(worker => worker.UseNewtonsoftJson())
+    .ConfigureServices(services =>
+    {
+
+        services.AddDbContextFactory<ApplicationDbContext>(options => {
+            var connection = builder.Configuration.GetConnectionString("Database");
+            options.UseSqlServer(connection);
+        });
+
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        services.AddSingleton<IApplicationService, ApplicationService>();        
+        services.AddHostedService<ApplicationService>();
+    })
+    .ConfigureAppConfiguration(builder =>
+    {
+        try {
+            Logger.Instance.LogInformation("ConfigureAppConfiguration()", "ConfigureAppConfiguration()");
+
+            if (ConfigurationModel.env != Environments.Development)
+            {
+                builder.AddAzureAppConfiguration(options =>
+                {
+                    var connectionString = Environment.GetEnvironmentVariable("AppConfig", EnvironmentVariableTarget.Process);
+                    Logger.Instance.LogInformation("ConfigureAppConfiguration().AddAzureAppConfiguration()", $"AppConfig ConnectionString: {connectionString}");
+                    options.Connect(new Uri(connectionString), new ManagedIdentityCredential())
+                    .ConfigureKeyVault(kv =>
+                    {
+                        kv.SetCredential(new DefaultAzureCredential());
+                    }).Select(KeyFilter.Any, "NonSecret").Select(KeyFilter.Any, "Secret");
+                });
+            }
+        }
+        catch(Exception e) {
+            Logger.Instance.LogException(e, "ConfigureAppConfiguration()", "ConfigureAppConfiguration()");
+        }
+    })
+    .Build();
+
+host.Run();
