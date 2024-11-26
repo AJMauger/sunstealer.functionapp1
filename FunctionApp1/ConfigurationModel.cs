cf8b19a6c198c10987920d3955fe09f1f22bbfeb
My name. <40044228+AJMauger@users.noreply.github.com>
2024-11-25 22:05:34 -0700

    Delete FunctionApp1 directory

--- FunctionApp1/ConfigurationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FunctionApp1.Models;
-
-public class ConfigurationModel
-{
-    public static string env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Error";
-
-    public string DatabaseConnectionString { get; set; } = string.Empty;
-}
